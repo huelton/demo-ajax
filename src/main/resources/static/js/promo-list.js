0a424ca937b497c76b85058348f650cf4b0b29e9
Huelton Dihon <hueltondihon@gmail.com>
2019-03-04 15:40:06 -0300

    Criando o Infinite Scroll

---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,12 @@
+// Efeito infinite scroll
+$(window).scroll(function() {
+	
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	
+	console.log('scrollTop', scrollTop, ' | ', 'conteudo', conteudo );
+	
+	if(scrollTop >= conteudo) {
+		console.log('***');
+	}
+});
