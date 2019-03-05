69ad637d8f13286dc73d0bdb16e0b39e099c9691
Huelton Dihon <hueltondihon@gmail.com>
2019-03-05 11:11:45 -0300

    O metodo Ajax para likes

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,4 +1,3 @@
-
 var pageNumber = 0;
 
 $(document).ready(function() {
@@ -6,59 +5,74 @@ $(document).ready(function() {
 	$("#fim-btn").hide();
 });
 
-//Efeito infinite scroll
+// Efeito infinite scroll
 $(window).scroll(function() {
-	
+
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
-	
+
 	// console.log('scrollTop: ', scrollTop, ' | ', 'conteudo', conteudo );
-	
-	if(scrollTop >= conteudo) {
-		pageNumber ++;		
-		setTimeout(function () {
+
+	if (scrollTop >= conteudo) {
+		pageNumber++;
+		setTimeout(function() {
 			loadByScrollBar(pageNumber);
 		}, 200);
 	}
 });
 
-
 function loadByScrollBar(pageNumber) {
-	
+
 	$.ajax({
-		method: "GET",
-		url: "/promocao/list/ajax",
-		data: {
-			page: pageNumber
+		method : "GET",
+		url : "/promocao/list/ajax",
+		data : {
+			page : pageNumber
 		},
-		beforeSend: function() {
+		beforeSend : function() {
 			$("#loader-img").show();
 		},
-		success: function(response) {
+		success : function(response) {
 			// console.log("resposta > ", response)
-			//150 é utilizado para trazer pouca informações e sair do if
-           if(response.length > 150){
-			
-			$(".row").append( $(response).hide().fadeIn(400) );
-			
-           }else {
-        	   $("#loader-img").removeClass("loader");
-        	   $("#fim-btn").show();
-        	   
-           }
+			// 150 é utilizado para trazer pouca informações e sair do if
+			if (response.length > 150) {
+
+				$(".row").append($(response).hide().fadeIn(400));
+
+			} else {
+				$("#loader-img").removeClass("loader");
+				$("#fim-btn").show();
+
+			}
 		},
-		error: function(xhr) {
-			alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+		error : function(xhr) {
+			alert("Ops, ocorreu um erro: " + xhr.status + " - "
+					+ xhr.statusText);
 		},
-		complete: function() {
+		complete : function() {
 			$("#loader-img").hide();
 		}
 	})
 }
 
 // Adicionar Likes
+$(document).on(
+		"click",
+		"button[id*='likes-btn-']",
+		function() {
+			var id = $(this).attr("id").split("-")[2];
+			console.log("id: ", id);
 
-$("button[id*='likes-btn-").on("click", function() {
-	var id = $(this).attr("id").split("-")[2];
-console.log("id: ", id);
-});
+			$.ajax({
+				method : "POST",
+				url : "/promocao/like/" + id,
+				success : function(response) {
+					$("#likes-count-" + id).text(response);
+				},
+				error : function(xhr) {
+					alert("Ops, ocorreu um erro: " + xhr.status + ", "
+							+ xhr.statusText);
+				}
+			});
+
+		});
