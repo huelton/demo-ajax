c5a423df04fa173171ddafc7a048589d8312a374
Huelton Dihon <hueltondihon@gmail.com>
2019-03-04 23:17:02 -0300

    Infinite scroll em ação

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,13 +1,18 @@
 
 var pageNumber = 0;
 
+$(document).ready(function() {
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+});
+
 //Efeito infinite scroll
 $(window).scroll(function() {
 	
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
 	
-	console.log('scrollTop: ', scrollTop, ' | ', 'conteudo', conteudo );
+	// console.log('scrollTop: ', scrollTop, ' | ', 'conteudo', conteudo );
 	
 	if(scrollTop >= conteudo) {
 		pageNumber ++;		
@@ -26,9 +31,28 @@ function loadByScrollBar(pageNumber) {
 		data: {
 			page: pageNumber
 		},
+		beforeSend: function() {
+			$("#loader-img").show();
+		},
 		success: function(response) {
-			console.log("resposta > ", response)
-
+			// console.log("resposta > ", response)
+			//150 é utilizado para trazer pouca informações e sair do if
+           if(response.length > 150){
+			
+			$(".row").fadeIn(250, function() {
+        	   $(this).append(response);
+           });
+			
+           }else {
+        	   $("#fim-btn").show();
+        	   $("#loader-img").removeClass("loader");
+           }
+		},
+		error: function(xhr) {
+			alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();
 		}
 	})
 }
